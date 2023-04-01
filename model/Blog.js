c3dfbf9f4811b235b40f7fae8bc8a91aa34a3705
Sivaranjani-A <ranjani731997@gmail.com>
2023-04-01 21:00:13 +0530

    initial commit

---
+++ model/Blog.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  imageURL: String,
+  userId: String,
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: Object,
+});
+
+export default mongoose.model("Blogs", BlogSchema);
